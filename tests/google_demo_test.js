c346eb72be36be0629c2893b50d8b8c8f067e9a8
yamashiro.ryota <yamashiro0110@gmail.com>
2019-03-29 01:54:47 +0900

    add cookie test

--- tests/google_demo_test.js
+++ tests/google_demo_test.js
@@ -1,15 +1,25 @@
 module.exports = {
-  'Demo test Google' : function (client) {
-    client
+  'Demo test Google': function (browser) {
+    browser
       .url('https://www.google.com')
       .waitForElementVisible('body', 1000)
       .assert.title('Google')
       .assert.visible('input[type=text]')
       .setValue('input[type=text]', 'rembrandt van rijn')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
+      // .waitForElementVisible('button[name=btnG]', 1000)
+      // .click('button[name=btnG]')
+      // .pause(1000)
+      // .assert.containsText('ol#rso li:first-child', 'Rembrandt - Wikipedia')
+      .end();
+  },
+  'cookie test': function (browser) {
+    browser
+      .url('https://ogp-sample.netlify.com/')
+      .waitForElementVisible('body', 1000)
+      .getCookies(function (result) {
+        console.log(result);
+      })
       .pause(1000)
-      .assert.containsText('ol#rso li:first-child', 'Rembrandt - Wikipedia')
       .end();
   }
 };
